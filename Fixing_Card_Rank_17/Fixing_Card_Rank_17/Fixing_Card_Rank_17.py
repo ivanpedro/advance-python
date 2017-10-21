c58c7aa131fa34d5b21875520c6de83715cfe05d
ivan pedro <ivanpedrouk@gmail.com>
2017-10-21 21:05:16 +0100

    *

---
+++ Fixing_Card_Rank_17/Fixing_Card_Rank_17/Fixing_Card_Rank_17.py
@@ -0,0 +1,16 @@
+
+# -----------
+# User Instructions
+# 
+# Modify the card_ranks() function so that cards with
+# rank of ten, jack, queen, king, or ace (T, J, Q, K, A)
+# are handled correctly. Do this by mapping 'T' to 10, 
+# 'J' to 11, etc...
+
+def card_ranks(hand):
+    "Return a list of the ranks, sorted with higher first."
+    ranks = ['--123456789TJQKA'.index(r) for r,s in hand]
+    ranks.sort(reverse=True)
+    return ranks
+
+print card_ranks(['AC', '3D', '4S', 'KH']) #should output [14, 13, 4, 3]
--- poker/poker/poker.py
+++ poker/poker/poker.py
@@ -29,22 +29,26 @@ def hand_rank(hand):
     "return a value indicationg the ranking of a hand."
     ranks = card_ranks(hand)
     if straight (ranks) and flush(hand):
-        return (8, max(ranks))
+        return (8, max(ranks))# 2 3 4 5 6 (8,6) 6 7 8 9 T (8, 10)
     elif kind (4 , ranks):
         return (7, kind(4,ranks), kind(1,ranks))#9 9 9 9 3 9(7,9,3)
-    
-    return 
+    elif :
 
 
 def test():
-    "Test cases for the functions in porker Program."
-    sf = "6C 7C 8C 9C TC".split()
-    fk = "9D 9H 9S 9C 7D".split()
-    fh = "TD TC TH 7C 7D".split()
+    "Test cases for the functions in poker program"
+    sf = "6C 7C 8C 9C TC".split() # Straight Flush
+    fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
+    fh = "TD TC TH 7C 7D".split() # Full House
     assert poker([sf, fk, fh]) == sf
     assert poker([fk, fh]) == fk
     assert poker([fh, fh]) == fh
-    return "tests pass"
-
+    assert poker([sf]) == sf
+    assert poker([sf] + 99*[fh]) == sf
+    # For example, calling hand_rank on sf should output (8, 10)
+    assert hand_rank([sf]) == (8,10)
+    assert hand_rank([sf]) == (7,9,7)
+    assert hand_rank([sf]) == (6,10,7)
+    return "test pass"
 print (test())
 print (max([3,4,5,0]), max([3,2,-5,4],key = abs))
